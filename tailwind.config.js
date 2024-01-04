c6f8b78946d37e91a6839e3a70de167135f41fcb
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2024-01-04 20:32:31 +0100

    design the hero section and the about us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend:{
+      colors:{
+        'primary': '#DDE2FF',
+        'secondary': '#1A033B',
+        'word': '#202020'
+      }
+    },
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
